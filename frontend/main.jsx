6f19b3b062fab5a401d3e8ad839e71d77382b611
Horngy <mynameistehyeehong@icloud.com>
2025-10-12 02:08:13 +1100

    render

---
+++ frontend/main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import StockForecastDashboard from "./app.jsx";
+import "./styles.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<StockForecastDashboard />);
